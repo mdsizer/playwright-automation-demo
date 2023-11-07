a4a7b6bbacc834e45647cecefa498edd333203a5
Md. Sizer Ahmad <sizer.ahmad@enosisbd.com>
2023-11-08 01:47:50 +0600

    initialized basic framework and developed first scenario

---
+++ tests/features/login.feature
@@ -0,0 +1,10 @@
+Feature: Verify the user authentication
+
+Background:
+    Given User navigates to the login page
+
+Scenario: User should log in successfully
+    Given User types the username as "standard_user"
+    And User types the password as "secret_sauce"
+    When User clicks the login button
+    Then Login should be successful
---
+++ tests/steps/loginStep.ts
@@ -0,0 +1,29 @@
+import { Given, When, Then } from "@cucumber/cucumber"
+import { chromium, Page, Browser } from "@playwright/test"
+
+let browser: Browser
+let page: Page
+
+Given('User navigates to the login page', async function () {
+    browser = await chromium.launch({ headless: false })
+    page = await browser.newPage()
+    await page.goto("https://www.saucedemo.com/")
+})
+
+Given('User types the username as {string}', async function (userName) {
+    await page.locator("#user-name").fill(userName)
+})
+
+Given('User types the password as {string}', async function (password) {
+    await page.locator("#password").fill(password)
+})
+
+When('User clicks the login button', async function () {
+    await page.locator("#login-button").click()
+})
+
+Then('Login should be successful', async function () {
+    const loginValidationText = await page.locator(".app_logo").textContent()
+    console.log("Text: " + loginValidationText + " observed. Login Successful!")
+    await browser.close()
+})
